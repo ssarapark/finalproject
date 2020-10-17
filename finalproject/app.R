d9232b935c762b30d01d4b42b04b54fbadf4579a
Your Name <sarapark@Saras-MacBook-Pro-2.local>
2020-10-16 22:18:54 -0400

    Milestone done I think

--- finalproject/app.R
+++ finalproject/app.R
@@ -13,17 +13,26 @@ library(tidyverse)
 library(dplyr, warn.conflicts = FALSE)
 library(ggforce)
 
-d <- read_csv("DP_LIVE_14102020222633846.csv")
+oecd <- read_csv("DP_LIVE_14102020222633846.csv", 
+                 col_types = cols(LOCATION = col_character(),
+                                   INDICATOR = col_character(),
+                                   SUBJECT = col_character(),
+                                   MEASURE = col_character(),
+                                   FREQUENCY = col_character(),
+                                   TIME = col_double(),
+                                   Value = col_double(),
+                                   'Flag Codes' = col_logical())) %>% 
+    select(LOCATION, SUBJECT, TIME, Value) 
 
-# Define UI for application that draws a histogram
+
 ui <- navbarPage(
     "Final Project Title",
     tabPanel("Background", 
              titlePanel("Introduction")),
     tabPanel("Model",
              fluidPage(
-                 selectInput("x", "X variable", choices = names(d)),
-                 selectInput("y", "Y variable", choices = names(d)),
+                 selectInput("x", "X variable", choices = names(oecd)),
+                 selectInput("y", "Y variable", choices = names(oecd)),
                  selectInput("geom", "geom", c("point", "column", "jitter")),
                  plotOutput("plot")
              )),
@@ -49,9 +58,9 @@ ui <- navbarPage(
              h3("Data"),
              h4("Dataset 1: Violence Against Women, Attitudes towards violence, 
                 Percentage, 2014 or latest available"),
-             p("I am still in the process of cleaning and gathering data. I plan
-               to use dataset 1 by selecting specific countries, type of 
-               violence, and measure of violence.", a("OECD data", 
+             p("I am still in the process of cleaning and gathering data. 
+               I plan to use dataset 1 by selecting specific countries, type 
+               of violence, and measure of violence.", a("OECD data", 
                  href = 
                 "https://data.oecd.org/inequality/violence-against-women.htm")),
              h4("Dataset 2: Proportion of ever-partnered women and girls aged
@@ -80,17 +89,22 @@ ui <- navbarPage(
 server <- function(input, output, session) {
     plot_geom <- reactive({
         switch(input$geom,
-               point = geom_point(),
+               point = geom_point(alpha = 0.5),
                column = geom_col,
                jitter = geom_jitter()
         )
     })
     
     output$plot <- renderPlot({
-        ggplot(d, aes(.data[[input$x]], .data[[input$y]])) +
+        ggplot(oecd, aes(.data[[input$x]], .data[[input$y]], 
+                         label = LOCATION)) +
             plot_geom()
     }, res = 96)
 }
+
+oecd %>% 
+    ggplot(aes(x = SUBJECT, y = Value)) +
+    geom_text(aes(label = LOCATION))
 
 # Run the application 
 shinyApp(ui = ui, server = server)
